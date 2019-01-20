8bdac46f338ff9e6db882a9610ffbe1eef0d920d
Billy Pierce <wwpierce020602@gmail.com>
2019-01-20 00:51:06 -0800

    added basic hexagonal display

--- test.py
+++ test.py
@@ -1,27 +1,28 @@
 import sys
 import pygame
+import math
+import hex_lib
 pygame.init()
 
-size = width, height = 320, 240
-speed = [2, 2]
-black = 0, 0, 0
+size = width, height = 960, 720
+black = (0, 0, 0)
+white = (255, 255, 255)
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+def flat_hex_corner(centerx, centery, size, i): # From https://www.redblobgames.com/grids/hexagons/
+    angle_deg = 60 * i
+    angle_rad = (math.pi/180) * angle_deg
+    # return hex_lib.Point(centerx + size * math.cos(angle_rad),centery+size*math.sin(angle_rad))
+    return [centerx + size * math.cos(angle_rad), centery+size*math.sin(angle_rad)]
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-
-    screen.fill(black)
-    screen.blit(ball, ballrect)
+    
+    screen.fill(white)
+    hex_points = [flat_hex_corner(100, 100, 25, i) for i in range(0, 6)]
+    pygame.draw.lines(screen, black, True, hex_points, 2)
+    # screen.blit(ball, ballrect)
     pygame.display.flip()
