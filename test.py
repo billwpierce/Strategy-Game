1754099810ff6ab4b39d588025d0b705414952c9
Billy Pierce <wwpierce020602@gmail.com>
2019-01-20 21:15:51 -0800

    added hexmap

--- test.py
+++ test.py
@@ -1,28 +1,54 @@
 import sys
 import pygame
 import math
-import hex_lib
+import hex_lib as hlib
 pygame.init()
 
-size = width, height = 960, 720
-black = (0, 0, 0)
-white = (255, 255, 255)
+SIZE = WIDTH, HEIGHT = 1280, 960
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+LIGHT_GRAY = (220, 220, 220)
 
-screen = pygame.display.set_mode(size)
+SCREEN = pygame.display.set_mode(SIZE)
 
-def flat_hex_corner(centerx, centery, size, i): # From https://www.redblobgames.com/grids/hexagons/
+HEX_SIZE = 35
+MAP_RADIUS = 6
+MAP = []
+
+LAYOUT = hlib.Layout(hlib.layout_flat, hlib.Point(HEX_SIZE, HEX_SIZE), hlib.Point(WIDTH/2, HEIGHT/2))
+
+for q in range(-MAP_RADIUS, MAP_RADIUS+1): # From redblobgames
+    r1 = max(-MAP_RADIUS, -q - MAP_RADIUS)
+    r2 = min(MAP_RADIUS, -q + MAP_RADIUS)
+    for r in range(r1, r2+1):
+        MAP.append(hlib.Hex(q, r, -q-r))
+
+def flat_hex_corner(center, size, i): # From https://www.redblobgames.com/grids/hexagons/
     angle_deg = 60 * i
-    angle_rad = (math.pi/180) * angle_deg
-    # return hex_lib.Point(centerx + size * math.cos(angle_rad),centery+size*math.sin(angle_rad))
-    return [centerx + size * math.cos(angle_rad), centery+size*math.sin(angle_rad)]
+    angle_rad = math.pi / 180 * angle_deg
+    return [center.x + size * math.cos(angle_rad), center.y+size*math.sin(angle_rad)]
+
+def flat_hex_points(center, size):
+    return [flat_hex_corner(center, size, i) for i in range(0, 6)]
+
+def draw_hex(layout, hex, color=BLACK, width=2):
+    pygame.draw.polygon(SCREEN, color, flat_hex_points(hlib.hex_to_pixel(layout, hex), HEX_SIZE), width)
+
+def mouse_to_point(mouse):
+    return hlib.Point(mouse[0], mouse[1])
+
+def mouse_to_hex(mouse):
+    return hlib.hex_round(hlib.pixel_to_hex(LAYOUT, mouse_to_point(mouse)))
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
     
-    screen.fill(white)
-    hex_points = [flat_hex_corner(100, 100, 25, i) for i in range(0, 6)]
-    pygame.draw.lines(screen, black, True, hex_points, 2)
-    # screen.blit(ball, ballrect)
+    SCREEN.fill(WHITE)
+    if mouse_to_hex(pygame.mouse.get_pos()) in MAP:
+        draw_hex(LAYOUT, mouse_to_hex(pygame.mouse.get_pos()), width=0, color=LIGHT_GRAY)
+    
+    [draw_hex(LAYOUT, tile) for tile in MAP]
+    print()
     pygame.display.flip()
